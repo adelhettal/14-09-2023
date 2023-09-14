97c0bafe6a79c78c0641fc9271ef3b3d194f045b
Hettal Adel <120855016+adelhettal@users.noreply.github.com>
2023-09-14 18:23:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let X = { value: 10 };
+let y = x
+x.value = 20;
+let number = 10;
+function increase (number)
+{
+number++;
+}
+increase (number);
+console. log (number);
+let obj = { value: 10 };
+function increase (obj)
+obj.value++;
+I
+increase (obj); I
+console. log (obj) ;
+
+const circle
+{
+radius: 1,
+draw() {
+console. log ('draw');
+};
